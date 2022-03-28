a75f7cae25646fcdca96bbbc5adf0cf021a190e2
saman <saman.rafiee.rs@gmail.com>
2022-03-28 10:56:06 +0430

    creae settings files

---
+++ config/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$SETTINGS = [
+    'PUBLIC_DIR' => 'http://' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\', '/', dirname(__DIR__) . '/public')),
+    'ROOT' => 'http://' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\', '/', dirname(__DIR__))),
+
+    /*
+     * DataBase Setting
+     */
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'mvc',
+    'DB_USER' => 'root',
+    'DB_PASSWORD' => '',
+    'DB_CHARSET' => 'utf8',
+
+    /*
+     * Default Language
+     */
+    'LAN' => 'fa',
+
+    /*
+     * Application Name
+     */
+    'APP_NAME' => 'MVC',
+];
